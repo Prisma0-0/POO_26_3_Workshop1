65179ff51cab41179616d09be4961ed7feb82a70
Prisma0-0 <gdlealc@udistrital.edu.co>
2026-09-03 10:43:07 -0500

    metodo secuencia de fibonacci

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -58,11 +58,25 @@ public class Workshop {
 
     // Método que genera una serie de Fibonacci
     public int[] serieFibonacci(int n) {
-        // TODO: Implementar el método para generar la serie de Fibonacci hasta el número n.
-        // Ejemplo: Si n = 5, el resultado debería ser [0, 1, 1, 2, 3].
-        // Lanzar IllegalArgumentException si n es negativo.
+    if (n < 0) {
+        throw new IllegalArgumentException("El número de términos no puede ser negativo");
+    }
+
+    if (n == 0) {
         return new int[0];
     }
+    int[] resultado = new int[n];
+    resultado[0] = 0;
+
+    if (n > 1) {
+        resultado[1] = 1;
+        for (int i = 2; i < n; i++) {
+            resultado[i] = resultado[i - 1] + resultado[i - 2]
+        }
+    }
+
+    return resultado;
+}
 
     // Método que suma todos los elementos de un arreglo
     public int sumaElementos(int[] arreglo) {
